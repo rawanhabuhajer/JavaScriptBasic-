a8e7c07331f64d6d9441abfba592ed9a2f7eb17b
rawan6922&9 <rawanh.abuhajer@gmail.com>
2023-04-26 14:58:57 +0300

    add task

---
+++ h.js
@@ -0,0 +1,92 @@
+var cash = 1000;
+var currentLiabilities = 500;
+    console.log( cash / currentLiabilities);
+
+let revenues= 1000
+let expenses= 500
+    console.log( revenues/2);
+
+let liabilities= 1000
+let equity= 500
+    console.log(liabilities+equity);
+
+let profit= 1000
+let sales= 500
+    console.log(profit * sales);
+
+let numb1= 7 
+    numb2=9 
+    numb3=2
+    console.log((numb1+numb2+numb3)/3);
+
+let price= 150, 
+discount= 30
+console.log(price-(price*0.3));
+
+
+ let age =20;
+ console.log(age>18 || age<30);
+
+let number1= 2, 
+    number2= 3
+    console.log(2**3);
+
+let number3= 10, 
+    number4= 4
+    console.log(number3 % number4);
+
+    console.log(typeof(100)); 
+    console.log(typeof(73.9)); 
+    console.log(typeof(NaN)); 
+    console.log(typeof("Water")); 
+    console.log(typeof(false)); 
+    console.log(typeof(9 != 11)); 
+    console.log("Orang" + "e");
+    console.log("Orange" - "s");
+    console.log("4" + "8");
+    console.log( "4" - "8");
+    console.log("name" + 3);
+    console.log("name" - 3);
+    console.log( 82 * "word");
+    console.log(1 + "hello");
+    console.log("hello" + 1);
+    console.log(1 + true);
+    console.log("hello" + true);
+    console.log(typeof (Infinity));
+    console.log(1 == '1');
+    console.log(1 === '1');
+    
+    
+    console.log("WELCOME TO ORANGE");
+    console.log("TO");
+    console.log("Hello from Orange");
+    console.log("welcome to orange");
+    console.log("17");
+    console.log("Welcome to “Orange”");
+    console.log("Welcome to Orange Jordan");
+    
+    let y = "c";
+    let x = "actus";
+    let result = x.replaceAll("c", "*");
+    console.log(y + result);
+
+     
+    
+     
+     
+    
+     
+     
+     
+     
+     
+     
+     
+
+
+
+
+
+
+
+
